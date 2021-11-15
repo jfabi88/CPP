0cce87a9da2e16c6fd77d959bb2a7ef981062ab1
Jacopo Fabi <jfabi@e3r3p5.42roma.it>
2021-11-15 18:36:04 +0100

    jfabi 20211115 18:36

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfabi <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 17:45:00 by jfabi             #+#    #+#             */
+/*   Updated: 2021/11/15 17:45:02 by jfabi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class OutOfRangeException : public std::exception
+{
+    const char *what() const throw()
+    {
+        return ("Value not found");
+    }
+};
+
+template <class T>
+typename T::iterator easyfind(T container, int n)
+{
+    typename T::iterator it;
+    it = std::find(container.begin(), container.end(), n);
+    if (it == container.end())
+        throw OutOfRangeException();
+    return (it);
+}
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfabi <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 17:44:45 by jfabi             #+#    #+#             */
+/*   Updated: 2021/11/15 17:44:47 by jfabi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <list>
+#include <iostream>
+
+int main()
+{
+    std::list<int>::iterator tmp;
+    std::list<int> my_list = { 5, 16, 9, 0};
+    try
+    {
+        tmp = easyfind(my_list, 2);
+        std::cout << *tmp << std::endl; 
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        tmp = easyfind(my_list, 9);
+        std::cout << *tmp << std::endl; 
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
